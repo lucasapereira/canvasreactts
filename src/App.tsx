31214020f03e41a4e9c4309939186f0ed34a4638
Lucas <lucas.araujo.pereira@gmail.com>
2022-02-01 12:02:43 -0300

    feat: almost ready

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,16 @@ let ballSpeedX: number = 10;
 let ballSpeedY: number = 4;
 
 let paddle1Y = 250;
+let paddle2Y = 250;
+
+let player1Score = 0;
+let player2Score = 0;
+
+let showWinningScreen = false;
+
 const PADDLE_HEIGHT = 100;
+const PADDLE_THICKNESS = 10;
+const WINNING_SCORE = 3;
 
 function App() {
   const draw = (
@@ -23,7 +32,6 @@ function App() {
       let mouseX = evt.clientX - rect!.left - root.scrollLeft;
       let mouseY = evt.clientY - rect!.top - root.scrollTop;
 
-      console.log(mouseY);
       return {
         x: mouseX,
         y: mouseY,
@@ -34,11 +42,44 @@ function App() {
       drawEverything();
     };
 
+    const computerMovement = () => {
+      let paddle2YCenter = paddle2Y + PADDLE_HEIGHT / 2;
+
+      if (paddle2YCenter < ballY + 35) {
+        paddle2Y += 6;
+      } else if (paddle2YCenter > ballY - 35) {
+        paddle2Y -= 6;
+      }
+    };
+
     const moveEverything = () => {
+      if (showWinningScreen) {
+        return;
+      }
+      computerMovement();
       ballX = ballX + ballSpeedX;
 
-      if (ballX > canvas!.width || ballX < 0) {
-        ballSpeedX = -ballSpeedX;
+      if (ballX < 0) {
+        if (ballY < paddle1Y || ballY > paddle1Y + PADDLE_HEIGHT) {
+          player2Score += 1;
+          ballReset();
+        } else {
+          ballSpeedX = -ballSpeedX;
+
+          let deltaY = ballY - (paddle1Y + PADDLE_HEIGHT / 2);
+          ballSpeedY = deltaY * 0.35;
+        }
+      }
+
+      if (ballX > canvas!.width) {
+        if (ballY < paddle2Y || ballY > paddle2Y + PADDLE_HEIGHT) {
+          player1Score += 1;
+          ballReset();
+        } else {
+          ballSpeedX = -ballSpeedX;
+          let deltaY = ballY - (paddle2Y + PADDLE_HEIGHT / 2);
+          ballSpeedY = deltaY * 0.35;
+        }
       }
 
       ballY = ballY + ballSpeedY;
@@ -48,13 +89,41 @@ function App() {
       }
     };
 
+    const ballReset = () => {
+      ballX = canvas!.width / 2;
+      ballY = canvas!.height / 2;
+      ballSpeedX = -ballSpeedX;
+
+      if (player1Score === WINNING_SCORE || player2Score === WINNING_SCORE) {
+        player1Score = 0;
+        player2Score = 0;
+        showWinningScreen = true;
+      }
+    };
+
     const drawEverything = () => {
       ctx.fillStyle = "black";
       ctx.fillRect(0, 0, canvas!.width, canvas!.height);
 
-      colorRect(0, paddle1Y, 10, PADDLE_HEIGHT, "white");
+      if (showWinningScreen) {
+        return;
+      }
+
+      //do jogador
+      colorRect(0, paddle1Y, PADDLE_THICKNESS, PADDLE_HEIGHT, "white");
+
+      colorRect(
+        canvas!.width - PADDLE_THICKNESS,
+        paddle2Y,
+        PADDLE_THICKNESS,
+        PADDLE_HEIGHT,
+        "white"
+      );
 
       colorCircle(ballX, ballY, 10, "white");
+
+      ctx.fillText("Player 1: " + player1Score, 100, 100);
+      ctx.fillText("Player 2: " + player2Score, 600, 100);
     };
 
     const colorCircle = (
@@ -85,7 +154,6 @@ function App() {
     setInterval(() => callBoth(), 1000 / framesPerSecond);
 
     canvas?.addEventListener("mousemove", function (evt) {
-      console.log("aaa");
       var mousePos = calculateMousePos(evt);
       paddle1Y = mousePos.y - PADDLE_HEIGHT / 2;
     });
